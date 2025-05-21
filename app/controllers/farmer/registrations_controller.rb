2929c7971e32dd0ca68b93db5bf3cf29c7b9089b
EC2 Default User <ec2-user@ip-172-31-6-245.ap-northeast-1.compute.internal>
2025-05-21 13:15:42 +0000

    検索機能

--- app/controllers/farmer/registrations_controller.rb
+++ app/controllers/farmer/registrations_controller.rb
@@ -42,7 +42,7 @@ class Farmer::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :seller_address, :postal_code])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :seller_address, :postal_code, :prefecture])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
